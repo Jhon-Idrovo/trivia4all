a98bd7582351d92f2143f8409fee1476e5fdaa96
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-10-06 09:12:05 -0500

    removed category image

--- components/Categorie.jsx
+++ components/Categorie.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function Categorie({ name, handleClick }) {
   return (
     <button className="categorie" onClick={() => handleClick(name)}>
-      <img src="icons" alt="categorie image" />
       <h3>{name}</h3>
     </button>
   );
